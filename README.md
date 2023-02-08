eaf28a176761864cdbd2014be57db52760d17a5f
Logan Reese <lreese2112@gmail.com>
2023-02-08 04:34:04 +0000

    Lab02 requirements

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# about-me-201
+# about-me
+
+## Lab02
+
+This is a guessing game app that goes directly into my website.
+
+### Author: Logan Reese
+
+### Links
+
+### Lighthouse Score
+[LighthouseScore](AccessibilityScore.jpg)
+
+#### Reflection From Journal
+Every day I am learning something new in conjunction with things I have learned prior and refreshing out-of-practice skills. Today I learned about the template literal method for concatenating rather than plus signs in HTML. Using this going forward will make me more comfortable with concepts learned that I have never used before. In this way, I am planning and learning which learned skills I find most valuable and the ones that I don't think I would use unless directed to. Anything that I comprehend (which is a mound to a mountain of information right now) I will take with me and attempt at least once. As of now, I am evaluating every day what I want to learn within reading material and the course work as it proceeds because if I were to focus on what I truly want to know, Python, I would overwhelm myself and explode from knowledge overload. I am trusting the process and taking it one day at a time, with adult-sized baby steps. A shuffle if you will. 
--- js/app.js
+++ js/app.js
@@ -1,3 +1,54 @@
 'use strict';
 
-console.log('Zup Mundo');
+
+let userName = prompt('What is your name?');
+
+alert(`Welcome to my site ${userName}! Lets play a guessing game. Answer yes or no and dont attempt to break my site, cool?`);
+
+
+
+let questionOne = prompt('Are my eyes blue?').toLowerCase();
+if(questionOne === 'yes' || questionOne === 'y'){
+  alert('Yes they are!');
+} else if(questionOne === 'no' || questionOne === 'n'){
+  alert('Nope, guess again');
+}
+
+
+let questionTwo = prompt('Do I have kids?').toLowerCase();
+if(questionTwo === 'yes' || questionTwo === 'y'){
+  alert('Yes 3!');
+  //console.log("yes! 3");
+} else if(questionTwo === 'no' || questionTwo === 'n'){
+  alert('Nope, guess again');
+}
+
+
+let questionThree = prompt('Do I like chocolate?').toLowerCase();
+if(questionThree === 'yes' || questionThree === 'y'){
+  //console.log("sure does!");
+  alert('Does a religious bear poop in a church in the woods? Yep!');
+} else if(questionThree === 'no' || questionThree === 'n'){
+  alert('Nope, guess again');
+}
+
+
+let questionFour = prompt('Do I have a hairless cat?').toLowerCase();
+if(questionFour === 'no' || questionFour === 'n'){
+  alert('Sadly, you are correct.');
+} else if(questionFour === 'yes' || questionFour === 'y'){
+  alert('Nope, guess again');
+}
+
+
+let questionFive = prompt('Is my favorite band Gojira?').toLowerCase();
+if(questionFive === 'yes' || questionFive === 'y'){
+  alert('Yes! They are awesome!');
+} else if(questionFive === 'no' || questionFive === 'n'){
+  alert('Nope, guess again');
+}
+
+
+
+alert(`Thanks for playing my game ${userName}.. hope you like the site!`);
+
